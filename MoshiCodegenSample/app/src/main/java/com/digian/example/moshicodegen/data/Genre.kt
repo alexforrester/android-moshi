27c44cd9f9110295420884c30ed6093d8420dad1
alexforrester <aforresterwork@gmail.com>
2020-03-01 16:08:36 +0000

    Adds missing @JsonClass for codegen and updates moshi version for moshi codegen and reflection

--- MoshiCodegenSample/app/src/main/java/com/digian/example/moshicodegen/data/Genre.kt
+++ MoshiCodegenSample/app/src/main/java/com/digian/example/moshicodegen/data/Genre.kt
@@ -1,6 +1,9 @@
 package com.digian.example.moshicodegen.data
 
+import com.squareup.moshi.JsonClass
+
 /**
  * Created by Alex Forrester on 2019-04-26.
  */
+@JsonClass(generateAdapter = true)
 data class Genre(val id: Int, val name: String)
